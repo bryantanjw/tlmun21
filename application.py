18cbf587bd52254e46381833f07bd642e2ef3208
TheClub4 <bryantan-888@hotmail.com>
2020-11-01 22:59:01 +0800

    First commit

---
+++ application.py
@@ -0,0 +1,34 @@
+import os
+import requests
+from flask import Flask, session,render_template, redirect, url_for, request
+
+# Configure app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/councils")
+def councils():
+    return render_template('councils.html')
+
+@app.route("/timeline")
+def timeline():
+    return render_template('timeline.html')
+
+@app.route("/resources")
+def resources():
+    return render_template('resources.html')
+
+@app.route("/secretariat")
+def secretariat():
+    return render_template('secretariat.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
