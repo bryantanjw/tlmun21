fc88b80ef38730dd310dccd28fbbb379dc5f732e
TheClub4 <bryantan-888@hotmail.com>
2020-11-24 05:11:22 +0800

    Countdown Homepage

--- static/js/main.js
+++ static/js/main.js
@@ -70,7 +70,7 @@ $(document).ready(function(){
   });
 
 
-    // Mobile Navigation
+  // Mobile Navigation
   if ($('.nav-menu').length) {
     var $mobile_nav = $('.nav-menu').clone().prop({
       class: 'mobile-nav d-lg-none'
@@ -258,5 +258,36 @@ $(document).ready(function(){
   };
 
   $("#timeline-1").timeline();
+
+  const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let birthday = "Feb 26, 2021 00:00:00",
+countDown = new Date(birthday).getTime(),
+x = setInterval(function() {    
+
+  let now = new Date().getTime(),
+      distance = countDown - now;
+
+  document.getElementById("days").innerText = Math.floor(distance / (day)),
+    document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+    document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+    document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+  //do something later when date is reached
+  if (distance < 0) {
+    let headline = document.getElementById("headline"),
+        countdown = document.getElementById("countdown"),
+        content = document.getElementById("content");
+
+    countdown.style.display = "none";
+    content.style.display = "block";
+
+    clearInterval(x);
+  }
+  //seconds
+}, 0)
 
  });
